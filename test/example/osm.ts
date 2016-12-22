827e7799cb00c30dfdc873570adee282718b25a9
M. Hofman <m.hofman@chronos-technology.nl>
2016-12-22 01:14:13 +0100

    Add correct test file

--- test/example/osm.ts
+++ test/example/osm.ts
@@ -40,7 +40,7 @@ export namespace osm {
         export type image_use_gravatar = boolean;
         export type image_content_type = string;
         export type auth_provider = string;
-        export type uuid_column=string;
+        export type uuid_column = string;
     }
 
     export interface users {
