ba7339b3ff199f603f604db36fca654460ccbb39
Mark Crisp <macr1324@gmail.com>
2017-04-22 22:08:38 -0400

    remove unneeded code from cleanup script

--- test/fixture/postgres/initCleanup.sql
+++ test/fixture/postgres/initCleanup.sql
@@ -3,6 +3,3 @@ DROP TYPE IF EXISTS user_status_enum;
 DROP TYPE IF EXISTS format_enum;
 
 DROP SCHEMA IF EXISTS maxi CASCADE;
-DROP TABLE IF EXISTS maxi.users1;
-DROP TABLE IF EXISTS maxi.users2;
-DROP TABLE IF EXISTS maxi.users3;
