8d27cc0cfc49f548e9b128368df4eff193b8de36
George Zhao <zorjiz@gmail.com>
2018-12-13 12:16:29 +1100

    Updated reserved keywords obtained from https://github.com/Microsoft/TypeScript/issues/2536

--- src/typescript.ts
+++ src/typescript.ts
@@ -10,9 +10,71 @@ import Options from './options'
 
 function nameIsReservedKeyword (name: string): boolean {
     const reservedKeywords = [
-        'string',
+        // Reserved Words
+        'break',
+        'case',
+        'catch',
+        'class',
+        'const',
+        'continue',
+        'debugger',
+        'default',
+        'delete',
+        'do',
+        'else',
+        'enum',
+        'export',
+        'extends',
+        'false',
+        'finally',
+        'for',
+        'function',
+        'if',
+        'import',
+        'in',
+        'instanceof',
+        'new',
+        'null',
+        'return',
+        'super',
+        'switch',
+        'this',
+        'throw',
+        'true',
+        'try',
+        'typeof',
+        'var',
+        'void',
+        'while',
+        'with',
+
+        // Strict Mode Reserved Words
+        'as',
+        'implements',
+        'interface',
+        'let',
+        'package',
+        'private',
+        'protected',
+        'public',
+        'static',
+        'yield',
+
+        // Contextual Keywords
+        'any',
+        'boolean',
+        'constructor',
+        'declare',
+        'get',
+        'module',
+        'require',
         'number',
-        'package'
+        'set',
+        'string',
+        'symbol',
+        'type',
+        'from',
+        'of'
     ]
     return reservedKeywords.indexOf(name) !== -1
 }
